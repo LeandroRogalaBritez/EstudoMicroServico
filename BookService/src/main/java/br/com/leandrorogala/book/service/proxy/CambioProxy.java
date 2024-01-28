70091e3feada2c0943d6a4effa17aaa037f1b5f4
LeandroRogalaBritez <leandrorogala17@hotmail.com>
2024-01-28 16:24:49 -0300

    Corrigido CambioProxy para rodar no docker.

--- BookService/src/main/java/br/com/leandrorogala/book/service/proxy/CambioProxy.java
+++ BookService/src/main/java/br/com/leandrorogala/book/service/proxy/CambioProxy.java
@@ -5,7 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "cambio-service", url = "localhost:8765")
+@FeignClient(name = "cambio-service", url = "http://api-gateway:8765")
+//@FeignClient(name = "cambio-service", url = "localhost:8765")
 public interface CambioProxy {
 	
 	@GetMapping(value = "/cambio-service/{amount}/{from}/{to}")
