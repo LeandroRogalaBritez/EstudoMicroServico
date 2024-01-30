5a17df0ca674eb9e40d2a049c1b524dd900a0fa6
LeandroRogalaBritez <leandrorogala17@hotmail.com>
2024-01-30 17:58:03 -0300

    configuração da URL do CambioProxy localhost e docker

--- BookService/src/main/java/br/com/leandrorogala/book/service/proxy/CambioProxy.java
+++ BookService/src/main/java/br/com/leandrorogala/book/service/proxy/CambioProxy.java
@@ -5,10 +5,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "cambio-service", url = "http://api-gateway:8765")
-//@FeignClient(name = "cambio-service", url = "localhost:8765")
+@FeignClient(name = "cambio-service", url = "${cambio-service.url}")
 public interface CambioProxy {
-	
+
 	@GetMapping(value = "/cambio-service/{amount}/{from}/{to}")
 	public Cambio getCambio(
 			@PathVariable("amount") Double amount,
